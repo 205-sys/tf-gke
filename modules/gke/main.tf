f46157a072ddba3071189c93e7f18caadc50ce5e
205-sys <bunsengsan205@gmail.com>
2026-06-23 08:59:02 +0000

    udpate

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -4,7 +4,7 @@ resource "google_container_cluster" "gke" {
   network    = var.network
   subnetwork = var.subnetwork
 
-  deletion_protection = false
+  deletion_protection = true
 
   remove_default_node_pool = true
   initial_node_count       = 1
